e42081bf060598f3d52244a5dcd9e045985a5f2b
Timothée Poulain <54988228+TimotheePoulainPoly@users.noreply.github.com>
2022-11-13 19:43:31 +0100

    Added year and popularity to CSV

--- read_rds.r
+++ read_rds.r
@@ -12,7 +12,7 @@ colnames(songs_df)
 
 filtered_songs <- filter(songs_df, genre != "NULL" & bpm != "")
 
-mutated_songs <- filtered_songs %>% mutate(genres = strsplit(as.character(str_extract_all(genre, '"(.*)"')), ", ")) %>% mutate(key = str_extract(chords_metadata, '[A-G](b|)(maj|min|)')) %>% select(title, genres, bpm, length, gain, key)
+mutated_songs <- filtered_songs %>% mutate(genres = strsplit(as.character(str_extract_all(genre, '"(.*)"')), ", ")) %>% mutate(key = str_extract(chords_metadata, '[A-G](b|)(maj|min|)')) %>% select(title, genres, bpm, length, gain, key, rank, publicationDate)
 
 print(mutated_songs)
 
